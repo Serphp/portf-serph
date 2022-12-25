30befae0d5bd111f05405eb76bfd4dcaea774155
Bryan Rodriguez <Serphp@hotmail.com>
2022-12-25 10:14:40 -0500

    Change name to Navbar

--- Layout/User.jsx
+++ Layout/User.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import '../node_modules/bootswatch/dist/lux/bootstrap.min.css'
 import '../src/styles/Global.scss'
-import '../src/styles/Navbar.scss'
-import { Navbar } from "../components/Navbar";
+import '../src/styles/navbar.scss'
+import { Navbar } from "../components/navbar";
 
 
 const User = () => {
