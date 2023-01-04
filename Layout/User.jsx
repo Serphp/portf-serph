a7d70557a0d2b4c39114bd349808124f05a54fee
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-04 15:55:39 -0500

    Fix netlify 0.3

--- Layout/User.jsx
+++ Layout/User.jsx
@@ -2,18 +2,19 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import '../node_modules/bootswatch/dist/lux/bootstrap.min.css'
 import '../src/styles/Global.scss'
-import { Navbar } from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 
 const User = () => {
     return (
+        
     <>
-    
     <Navbar />
     <div className=""></div>
     <Outlet />
     <Footer />
     </>
+
     )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 
-export const Navbar = () => {
+export default function Navbar() {
+
     return (
     <>
         <nav>
