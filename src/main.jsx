e41e4ed27628a20aa6da7b42857a1ea85e579f79
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-05 06:09:45 -0500

    fixing deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import { RouterProvider } from "react-router-dom";
 import ConProvider from '../context/ConProvider.jsx'
 import { router } from '../router/router'
 
-ReactDOM.createRoot(document.getElementById("main")).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    // <React.StrictMode>
         <ConProvider>
         <RouterProvider router={router} />
         </ConProvider>
-    </React.StrictMode>
+    // </React.StrictMode>
 );
