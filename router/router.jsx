d93690ec59ab9169e084a291c65988a088de83dd
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-04 22:35:25 -0500

    removing unnecessary files

--- router/router.jsx
+++ router/router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom"
-import App from "../src/App"
 import About from "../pages/About"
 import User from "../Layout/User"
 import Home from "../pages/Home"
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-//import './index.css'
+
 import { RouterProvider } from "react-router-dom";
 import ConProvider from '../context/ConProvider.jsx'
 import { router } from '../router/router'
