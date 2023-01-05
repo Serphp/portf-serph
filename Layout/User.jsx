7a5a241e385509bec3be5a838a0f17e3a56d22a2
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-05 06:13:26 -0500

    remove navbar

--- Layout/User.jsx
+++ Layout/User.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
 
 //styles
 import '../node_modules/bootswatch/dist/lux/bootstrap.min.css'
 import '../src/styles/Global.scss'
-import '../src/styles/Navbar.scss'
+//import '../src/styles/Navbar.scss'
 
 
 const User = () => {
     return (
         
     <>
-    <Navbar />
+    {/* <Navbar /> */}
     <div className=""></div>
     <Outlet />
     <Footer />
