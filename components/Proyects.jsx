ca5e7bc850054c9fafac110f8292053be7e6f51a
Bryan Rodriguez <Serphp@hotmail.com>
2023-01-25 22:47:08 -0500

    [+/-] Fixes responsive web, Delete files unnecessary

--- components/Proyects.jsx
+++ components/Proyects.jsx
@@ -16,7 +16,7 @@ export default function Proyects() {
                 </div>
                 <div className="text-container">
                 <h2>{name}</h2>
-                <p style={{ 'fontSize': '16px'}}> {description}</p>
+                <p style={{ 'fontSize': '16px' }}> {description}</p>
                 <div className="lang">{lang}</div>
                 <div className="containerx">
                 <a href={url} target="_blank" className="button">Github</a>
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -14,15 +14,19 @@ export default function Home() {
 
 
         <section class="hello">
-        <div className="hello-text ">
-            <h1 style={{'textAlign': 'center'}}>Hola, soy Bryan </h1> 
-            <h4> SysAdmin | Web Dev | Tecnico en sistemas</h4>
-            {/* <p>Yo creo, que todos podemos lograr grandes cosas, si nos proponemos hacerlo.</p> */}
-        </div>
-        <div class="hello-img">
+            <div className="hello-text">
+                <h1 style={{'textAlign': 'center'}}>Hola, soy Bryan </h1> 
+                <h4> SysAdmin | Web Dev | Tecnico en sistemas</h4>
+            </div>
+            <div class="hello-img">
             <img src="https://media.licdn.com/dms/image/D4E03AQHk5VD69aRJGg/profile-displayphoto-shrink_800_800/0/1674619813456?e=1680134400&v=beta&t=ui5a91ZP2C0fZmH0syj_WEKSG8RNlgmTjgzOeDJZJQ8" alt="" />
-        </div>
-    </section>
+            </div>
+        </section>
+        {/* <section class="hello">
+            <div class="hello-text">
+                <p>Sobre mi</p>
+            </div>
+        </section> */}
         
         <Cards />
         </main>
