232c3936bd9f782953d7f3f59b90bc8de48b4ef4
Bryan Rodriguez <Serphp@hotmail.com>
2023-03-18 13:31:45 -0500

    Added loading page and improves global css/ Home ++

--- serph/NProyects.js
+++ serph/NProyects.js
@@ -28,20 +28,39 @@ export const NProyects = [
     },
     {
         id: 4,
-        name: 'Login and SignUP',
-        description: 'Pagina de Login y Registro de usuarios con Firebase y Vite',
+        name: 'Sign In and Sign Up',
+        description: 'Pagina de Login y Registro de usuarios con Firebase.',
         lang: 'React 16, ReactRouter, Vite, Context, Pico CSS, Firebase, Axios',
         image: 'https://raw.githubusercontent.com/Serphp/Serph-BaseAuth/master/src/assets/img/browser_QrlTSvhemR.png',
         url: 'https://github.com/Serphp/Serph-BaseAuth',
         url2: 'https://exquisite-speculoos-e07e7a.netlify.app/'
     },
+    // {
+    //     id: 5,
+    //     name: 'GunGale World SA:MP',
+    //     description: 'GGW fue un juego orientado en el Roleplay y Estilo libre fue trabajado bajo el lenguaje Pawno un lenguaje de script de alto nivel, similar al C.',
+    //     lang: ' C , Pawno ',
+    //     image: 'https://cdn.discordapp.com/attachments/493832893580312579/1030802314539302952/Sin_titulo-1.png',
+    //     url: 'https://github.com/Serphp/portf-serph',
+    //     url2: 'https://serph.netlify.app/'
+    // },
     {
-        id: 5,
-        name: 'GunGale World SA:MP',
+        id: 7,
+        name: 'Word press e-commerce',
+        description: 'Word press e-commerce de plantas para dar un ejemplo de como se puede hacer un e-commerce con word press',
+        lang: ' C , Pawno ',
+        image: 'https://cdn.discordapp.com/attachments/493832893580312579/1030802314539302952/Sin_titulo-1.png',
+        url: 'https://github.com/Serphp/portf-serph',
+        url2: 'https://serph.netlify.app/'
+    },
+    {
+        id: 8,
+        name: 'Word press Pagina web',
         description: 'GGW fue un juego orientado en el Roleplay y Estilo libre fue trabajado bajo el lenguaje Pawno un lenguaje de script de alto nivel, similar al C.',
         lang: ' C , Pawno ',
         image: 'https://cdn.discordapp.com/attachments/493832893580312579/1030802314539302952/Sin_titulo-1.png',
         url: 'https://github.com/Serphp/portf-serph',
         url2: 'https://serph.netlify.app/'
     },
+    
 ]
