d4e80b25cf70b1042c74897956fc85c8ef5b8911
Bryan Rodriguez <Serphp@hotmail.com>
2023-03-21 05:59:08 -0500

    Added ToggleTheme > Light/Dark > Adapting Styles to Darkmode

--- Layout/User.jsx
+++ Layout/User.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import Loading from '../components/Loading';
 import {
@@ -7,33 +7,46 @@ import {
 } from "../components"
 import '../node_modules/bootswatch/dist/lux/bootstrap.min.css'
 import '../src/styles/Global.scss'
-
-
-
+const InicialTheme = localStorage.getItem('theme') === 'dark';
 
 const User = () => {
-
+    const [theme, setTheme] = useState(InicialTheme);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
     setTimeout(() => {setIsLoading(false);}, 3000); // tiempo en milisegundos para mostrar el componente de "loading"
     setTimeout(function() { document.getElementById("loading").style.transform = "translateX(100%)";}, 3000);
     }, []);
+    
+
+    // useEffect(() => {
+    //     document.documentElement.setAttribute("data-theme", theme);
+    //     localStorage.setItem("theme", theme);
+    // }, [theme]);
+
+    useEffect(() => {
+        if (theme){
+            document.documentElement.setAttribute("data-theme", theme);
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", theme);
+            localStorage.setItem("theme", "light");
+        }
+    }, [theme]);
 
     return (  
     <>
     
     <Navbar />
     <div className="header">
-<<<<<<< HEAD
-        <input type="checkbox" id="dark-mode" />
-        <label for="dark-mode"></label>
-        
-=======
+
+    <input type="checkbox" id="dark-mode" onClick={() => setTheme(!theme)} />
+    <label for="dark-mode"></label>
+
     {/* <NavLink to="/"> Home </NavLink>
     <NavLink to="/Certificates"> Certificates </NavLink>
     <NavLink to="/proyectos"> Proyectos </NavLink> */}
->>>>>>> fdb93807a8e64fa8be9cdddd7dfbf67f40d96d2d
+
     <div className="headermenu">
     </div>
     <div className="titlen">
