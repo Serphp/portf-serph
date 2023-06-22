6d18618d9f0adc65b06168353c15134ae68bcd4b
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-21 19:11:11 -0500

    remove: Context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { RouterProvider } from "react-router-dom";
-import ConProvider from '../context/ConProvider.jsx'
 import { router } from '../router/router'
 //require('dotenv').config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     // <React.StrictMode>
-        <ConProvider>
         <RouterProvider router={router} />
-        </ConProvider>
     // </React.StrictMode>
 );
