4d6d0a413e89bbaa29422bfd53a43e5f53ddd95f
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-21 19:00:28 -0500

    fix: cards components

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -11,92 +11,79 @@ export default function Cards() {
     <div className='mb-5'></div>
 
     <div className="containerx">
-    <div className="cardx">
+    <div className="">
       <div className="face face1">
         <div className=" content">
         <i class="fa fa-door-open"></i>
           <h3> adaptación </h3>
         </div>
       </div>
-      <div className="face face2">
-      {/* <Link className="btncard" to="/about" type="button">Read More</Link> */}
-      </div>
     </div>
     {/* SEPARATE  */}
-    <div className="cardx">
+    <div className="">
     <div className="face face1">
         <div className=" content">
         <i class="fa fa-layer-group"></i>
           <h3> Organizado </h3>
         </div>
       </div>
-      <div className="face face2">
-      </div>
+
     </div>
         {/* SEPARATE  */}
-        <div className="cardx">
+        <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-spa"></i>
           <h3> Resiliencia </h3>
         </div>
-      </div>
-      <div className="face face2">
+
       </div>
     </div>
             {/* SEPARATE  */}
-            <div className="cardx">
+            <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-stack-overflow"></i>
           <h3> Autodidacta </h3>
         </div>
       </div>
-      <div className="face face2">
-      </div>
     </div>
             {/* SEPARATE  */}
-            <div className="cardx">
+            <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-users"></i>
           <h3> Comunicativo </h3>
         </div>
       </div>
-      <div className="face face2">
-      </div>
     </div>
         {/* SEPARATE  */}
-        <div className="cardx">
+        <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-chalkboard"></i>
           <h3> Aprendizaje </h3>
         </div>
       </div>
-      <div className="face face2">
-      </div>
+
     </div>
       {/* SEPARATE  */}
-        <div className="cardx">
+        <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-icons"></i>
         <h3> Creativo </h3>
         </div>
         </div>
-        <div className="face face2">
-        </div>
+
       </div>
       {/* SEPARATE  */}
-        <div className="cardx">
+        <div className="">
         <div className="face face1">
         <div className=" content">
         <i class="fa fa-hand-holding-heart"></i>
         <h3> Apasionado </h3>
         </div>
-        </div>
-        <div className="face face2">
         </div>
         </div>
         {/* END */}
