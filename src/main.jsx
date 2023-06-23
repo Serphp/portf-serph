15a5221c7b457f4cfea0f4cc15d9a43806a2adef
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-23 15:58:33 -0500

    add: @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react';
 
 import { RouterProvider } from "react-router-dom";
 import { router } from '../router/router'
@@ -8,6 +9,10 @@ import { router } from '../router/router'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     // <React.StrictMode>
-        <RouterProvider router={router} />
+    <>
+    <Analytics />
+    <RouterProvider router={router} />
+    </>
+
     // </React.StrictMode>
 );
