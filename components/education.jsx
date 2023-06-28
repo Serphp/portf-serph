3738ee161f3e8cdfbbbf9467a336d5f5156fdb79
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-28 18:22:10 -0500

    add: section education

---
+++ components/education.jsx
@@ -0,0 +1,29 @@
+const Education = () => {
+    return (
+        <div className="mainbg">
+        <h6 className="subtitle"><i class="fa fa-graduation-cap"></i> Formación Academica</h6>
+        <h2> Education </h2>
+        <div className="mb-5"></div>
+
+    <section className="educont">
+    <div className="edu"> <b>Lic. Administración en sistemas</b> en <b>Universidad del Istmo, Panamá </b>
+    <br/>
+    <small> Enero 2019 — Diciembre 2023</small>
+    <li> Proyecto Posgrado: destacado por tener una pagina web con la que se podria sustentar mediante dialogo, pero se mostro 
+        un mejor realismo de proyección de emprendimiento.
+    </li>
+    </div>
+
+    <div className="edu"> 
+    <b>Bachiller en Ciencias</b> en <b>Instituto Académico Panamá, INSTACAP</b> 
+    <br/>
+    <small> Marzo 2016 — Noviembre 2018</small>
+        <li>Estudiante elegido como Profesor de informatica por 1 día por 3 Años, para varios salones con diferentes edades y diferentes grados.</li>
+    </div>
+    </section>
+
+    </div>
+    );
+    }
+
+    export default Education;
