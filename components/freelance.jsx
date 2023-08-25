62c85a4eb1c49a9c63f32f84944ada612b461fe2
Bryan Rodriguez <serphp@hotmail.com>
2023-08-25 07:34:54 -0600

    fix: freelance section

--- components/freelance.jsx
+++ components/freelance.jsx
@@ -1,18 +1,37 @@
-const Freelance = () => {
+export default function Freelance() {
     return (
-    <div className="mainbg">
-    <h6 className="subtitle"><i class="fa fa-circle-dollar-to-slot"></i> Servicios profesionales </h6>
+ <>
+     <div className="mainbg">
+    <h6 className="subtitle"><i class="fa fa-box-open"></i> Servicios profesionales </h6>
     <h2> Freelance </h2>
     <div className="mb-5"></div>
 
     <section className="educont">
-        <div className="butoned">
-            test
-        </div>
+
+    <article className="free">
+            
+            <div className="obra">
+                Obra <span> <i class="fa fa-arrow-right"></i></span>
+            </div>
+            <div className="obra">
+                Contrato <span> <i class="fa fa-arrow-right"></i></span>
+                <div className="obrac">
+                    ola
+                </div>
+            </div>
+            
+            <div className="obra">
+                Trabajos realizados <span> <i class="fa fa-arrow-right"></i></span>
+                <div className="obrac">
+                    ola
+                </div>
+            </div>
+
+        </article>
+
     </section>
 
     </div>
+ </>
     );
     }
-
-    export default Freelance;
