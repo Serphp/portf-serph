d490d08cad5e2935a4a9fbebf483f4ad3d2efbd3
Bryan Rodriguez <Serphp@hotmail.com>
2023-10-06 11:32:20 -0500

    fix: improvement mobile style

--- components/form.jsx
+++ components/form.jsx
@@ -2,20 +2,29 @@
 export default function Formulario() {
     return (
     <>
-    <div className="mainbg">
-    <h6 className="subtitle"><i class="fa fa-box-open"></i> Servicios profesionales </h6>
-    <h2> Freelance </h2>
-    <div className="mb-5"></div>
+        <div className="mainbg">
+        <h6 className="subtitle"><i class="fa fa-box-open"></i> Contactame </h6>
+        <h2> Contact </h2>
+        <div className="mb-5"></div>
 
-<form >
-    <label>Name</label>
-    <input type="text" name="user_name" />
-    <label>Email</label>
-    <input type="email" name="user_email" />
-    <label>Message</label>
-    <textarea name="message" />
-    <input type="submit" value="Send" />
-</form>
+        <form >
+            <label>Name</label>
+            <input type="text" name="user_name" />
+
+            <br/>
+
+            <label>Email</label>
+            <input type="email" name="user_email" />
+
+            <br/>
+
+            <label>Message</label>
+            <textarea name="message" />
+
+            <br/>
+
+            <input type="submit" value="Send" />
+        </form>
 
 
     </div>
