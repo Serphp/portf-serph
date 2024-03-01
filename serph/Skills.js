03ff20534749f6d361910d956e8caaa8d86220f6
Bryan Rodriguez <Serphp@hotmail.com>
2024-03-01 00:18:16 -0500

    2024.5.3 : Fix errors, update Skills, Card component, update vitejs react

---
+++ serph/Skills.js
@@ -0,0 +1,44 @@
+export const Skills = [
+    {
+        id: '1',
+        icon: '',
+        habilidad: 'ADAPTACIÓN Y FLEXIBILIDAD'
+
+    },
+    {
+        id: '2',
+        icon: '',
+        habilidad: 'RESOLUCION DE PROBLEMAS'
+
+    },
+    {
+        id: '3',
+        icon: '',
+        habilidad: 'RESILIENCIA'
+    },
+    {
+        id: '4',
+        icon: '',
+        habilidad: 'AUTODIDACTA Y AUTÓNOMO'
+    },
+    {
+        id: '5',
+        icon: '',
+        habilidad: 'COMUNICATIVO Y DETALLADO'
+    },
+    {
+        id: '6',
+        icon: '',
+        habilidad: 'APRENDIZAJE CONSTANTE'
+    },
+    {
+        id: '7',
+        icon: '',
+        habilidad: 'CREATIVO E INNOVADOR'
+    },
+    {
+        id: '8',
+        icon: '',
+        habilidad: 'PROGRAMADOR APASIONADO'
+    },
+]
